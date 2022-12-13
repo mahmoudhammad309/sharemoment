5a35bed806b433599a587c72d0f5abc30befe34e
mahmoud <mahmoudhammad3009@gmail.com>
2022-12-13 19:48:56 +0200

    deploy

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { client } from '../client';
 import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
